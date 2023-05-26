22f1a76abb4265a118ac2d9e766b2a7bbcee1f8f
Pedro Henrique <pedrohs21@outlook.com>
2023-05-25 21:49:30 -0300

    Implementação inicial dos models.

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+LEVEL_CHOICES = (
+        ("1", "Fácil"),
+        ("2", "Médio"),
+        ("3", "Difícil")
+    )
+
+class Category(models.Model):
+    name = models.CharField("Nome da categoria", max_length=25)
+class Question(models.Model):
+    text = models.CharField("Texto da pergunta", max_length=400)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Categoria da pergunta")
+    level = models.CharField("Nível da pergunta", max_length=1, choices=LEVEL_CHOICES)
+    active = models.BooleanField("Pergunta está ativa?", default=True) # Define se a questão está ou não ativa para ser utilizada
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Pergunta")
+    text = models.CharField("Texto da opção de resposta", max_length=200)
+    answer = models.BooleanField("Resposta correta?", default=False) # Define se a pergunta é a resposta da questão
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.IntegerField("Pontuação do usuário")
+class UserAnswer(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.PROTECT)
+    date = models.DateTimeField(default=timezone.now)
+class Match(models.Model):
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField()
+    users = models.ManyToManyField(UserProfile, related_name="users_match")
+    winner = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="user_winner")
+    level = models.CharField("Nível da pergunta", max_length=1, choices=LEVEL_CHOICES)
+    categories = models.ManyToManyField(Category)
