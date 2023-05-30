cc58be5588b26a71e550e2d750856e075d8fef01
Pedro Henrique <pedrohs21@outlook.com>
2023-05-29 21:37:25 -0300

    Implementação da base da estrutura de requisição post js para responder as questões.

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.shortcuts import get_object_or_404
 from mainApp.models import Question
+from django.http import HttpResponse
+import json
 
 class AnswerQuestionView(TemplateView):
     """
@@ -14,6 +16,12 @@ class AnswerQuestionView(TemplateView):
     def get(self, request, *args, **kwargs):
         return super(AnswerQuestionView, self).get(request, *args, **kwargs)
     
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        answer = data.get("answer")
+        print(answer)
+        return HttpResponse(status=200)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['question'] = self.question
