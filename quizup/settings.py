d074622fd2b3f95b982511a8d78d9ee2c1c57f9b
Pedro Henrique <pedrohs21@outlook.com>
2023-06-07 21:03:45 -0300

    Configuração da página de login

--- quizup/settings.py
+++ quizup/settings.py
@@ -130,3 +130,7 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication
+
+# LOGIN_REDIRECT_URL = '' # Definir a página inicial do game
--- quizup/urls.py
+++ quizup/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+app_name = "quizup"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mainapp/', include('mainApp.urls')),
+    path('login/', auth_views.LoginView.as_view(), name="login")
 ]
