311856c332a7cc4cd4175d349ce2e42b1d4de7ec
Pedro Henrique <pedrohs21@outlook.com>
2024-09-15 16:53:00 -0300

    add first test cases

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from mainApp import views
+
+
+class MainAppURLsTest(TestCase):
+    def test_mainApp_home_url_is_correct(self):
+        url = reverse('mainapp:home')
+        self.assertEqual(url, '/')
+
+    def test_mainApp_match_url_is_correct(self):
+        url = reverse('mainapp:match', kwargs={'pk': 1})
+        self.assertEqual(url, '/match/1/')
+
+    def test_mainApp_answer_question_url_is_correct(self):
+        url = reverse('mainapp:answer_question')
+        self.assertEqual(url, '/answer-question/')
+
+    def test_mainApp_create_match_url_is_correct(self):
+        url = reverse('mainapp:create_match')
+        self.assertEqual(url, '/create-match/')
+
+    def test_mainApp_user_matches_url_is_correct(self):
+        url = reverse('mainapp:user_matches')
+        self.assertEqual(url, '/user-matches/')
+
+    def test_mainApp_import_questions_url_is_correct(self):
+        url = reverse('mainapp:import_questions')
+        self.assertEqual(url, '/import-questions/')
+
+    def test_mainApp_user_profile_url_is_correct(self):
+        url = reverse('mainapp:user_profile', kwargs={'username': 'user-name'})
+        self.assertEqual(url, '/profile/user-name')
+
+class MainAppViewTest(TestCase):
+    def test_home_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:home'))
+        self.assertIs(function.func.view_class, views.HomeView)
+
+    def test_match_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:match', kwargs={'pk': 1}))
+        self.assertIs(function.func.view_class, views.MatchView)
+
+    def test_answer_question_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:answer_question'))
+        self.assertIs(function.func.view_class, views.AnswerQuestionView)
+
+    def test_create_match_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:create_match'))
+        self.assertIs(function.func.view_class, views.CreateMatchView)
+
+    def test_user_matches_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:user_matches'))
+        self.assertIs(function.func.view_class, views.UserMatchesView)
+
+    def test_import_questions_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:import_questions'))
+        self.assertIs(function.func.view_class, views.ImportQuestionsView)
+
+    def test_user_profile_view_function_is_correct(self):
+        function = resolve(reverse('mainapp:user_profile', kwargs={'username': 'user-name'}))
+        self.assertIs(function.func.view_class, views.UserProfileView)
