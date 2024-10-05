fb90748ae44bd6c7565c08aa915ec601dfcd8388
Pedro Henrique <pedrohs21@outlook.com>
2024-10-05 15:54:18 -0300

    add `MainAppTestBase` class

---
+++ mainApp/tests/test_base.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+
+from mainApp.models import Category, Match, Question, UserProfile
+
+
+class MainAppTestBase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def make_user(self, username='my_user', password='my_pass'):
+        user = User.objects.create_user(username=username, password=password)
+        UserProfile.objects.create(user=user)
+        return user
+
+    def login(self, username='my_user', password='my_pass'):
+        self.client.login(username=username, password=password)
+
+    def make_logged_user(self, username='my_user', password='my_pass'):
+        user = self.make_user(username, password)
+        self.login(username, password)
+        return user
+
+    def make_category(self, name='category_name'):
+        return Category.objects.create(name=name)
+
+    def make_question(
+        self,
+        text='question_1_text',
+        category_data=None,
+        category=None,
+        level='1',
+        active=True,
+    ):
+        if category_data is None:
+            category_data = {}
+
+        if category is None:
+            category = self.make_category(**category_data)
+
+        return Question.objects.create(
+            text=text,
+            category=category,
+            level=level,
+            active=active,
+        )
+
+    def make_match(self, start_date=None, level='1', user=None, category=None):
+        if start_date is None:
+            start_date = timezone.now()
+
+        if category is None:
+            category = self.make_category()
+
+        if user is None:
+            user = self.make_user()
+
+        self.make_question(category=category)
+        self.make_question(category=category)
+        self.make_question(category=category)
+
+        match = Match.objects.create(
+            start_date=start_date,
+            level=level,
+        )
+        match.users.add(user)
+        match.categories.add(category)
+
+        return match
--- mainApp/tests/test_views.py
+++ mainApp/tests/test_views.py
@@ -1,44 +1,38 @@
-from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import resolve, reverse
 
 from mainApp import views
-from mainApp.models import UserProfile
+
+from .test_base import MainAppTestBase
 
 
-class MainAppViewTest(TestCase):
+class MainAppViewTest(MainAppTestBase):
     def test_home_view_function_is_correct(self):
         function = resolve(reverse('mainapp:home'))
         self.assertIs(function.func.view_class, views.HomeView)
 
     def test_home_view_returns_status_code_200(self):
-        username = 'my_user'
-        password = 'my_pass'
-        user = User.objects.create_user(username=username, password=password)
-        UserProfile.objects.create(user=user)
-        self.client.login(username=username, password=password)
+        self.make_logged_user()
         response = self.client.get(reverse('mainapp:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_home_view_loads_correct_template(self):
-        username = 'my_user'
-        password = 'my_pass'
-        user = User.objects.create_user(username=username, password=password)
-        UserProfile.objects.create(user=user)
-        self.client.login(username=username, password=password)
+        self.make_logged_user()
         response = self.client.get(reverse('mainapp:home'))
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_template_shows_no_matchs_found_if_no_matchs(self):
-        username = 'my_user'
-        password = 'my_pass'
-        user = User.objects.create_user(username=username, password=password)
-        UserProfile.objects.create(user=user)
-        self.client.login(username=username, password=password)
+    def test_home_template_shows_no_matches_found_if_no_matchs(self):
+        self.make_logged_user()
         response = self.client.get(reverse('mainapp:home'))
         self.assertIn(
             'Nenhuma partida encontrada!', response.content.decode('utf-8')
         )
+
+    def test_home_template_shows_matches(self):
+        user = self.make_logged_user()
+        match = self.make_match(user=user)
+        response = self.client.get(reverse('mainapp:home'))
+        response_matches = response.context['userMatches']
+        self.assertEqual(response_matches.first().id, match.id)
 
     def test_match_view_function_is_correct(self):
         function = resolve(reverse('mainapp:match', kwargs={'pk': 1}))
@@ -49,11 +43,7 @@ class MainAppViewTest(TestCase):
         self.assertIs(function.func.view_class, views.AnswerQuestionView)
 
     def test_answer_question_view_returns_404_if_match_not_found(self):
-        username = 'my_user'
-        password = 'my_pass'
-        user = User.objects.create_user(username=username, password=password)
-        UserProfile.objects.create(user=user)
-        self.client.login(username=username, password=password)
+        self.make_logged_user()
         response = self.client.get(
             reverse('mainapp:answer_question'), data={'match': 100}
         )
