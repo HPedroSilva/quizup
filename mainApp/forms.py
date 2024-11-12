a83d8c1cfc9995f48f37dda5439763063ec8c47c
Pedro Henrique <pedrohs21@outlook.com>
2024-11-11 21:58:26 -0300

    feat: add `/signup` route to users create their accounts

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ImportQuestionsForm(forms.Form):
     file = forms.FileField()
+
+
+class UserCreateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2')
