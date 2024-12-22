889bf203ccb20650174ffb106783645aa13ce625
Pedro Henrique <pedrohs21@outlook.com>
2024-12-21 21:57:03 -0300

    feat: add settings for vercel deployment

--- quizup/settings.py
+++ quizup/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from utils.environment import comma_separated_str_to_list
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
